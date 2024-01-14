c391c3092879e68f519a944d57d3fcb872dc302e
sufia1117 <122894175+sufia1117@users.noreply.github.com>
2024-01-14 14:53:20 -0500

    footer file referenced on each webpage

---
+++ footer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <style>
+        footer {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<?php
+define("FILE_AUTHOR", 'S. Khan');
+echo "<hr>";
+echo "<footer>";
+    echo "<small> (C) " . FILE_AUTHOR . ", 2023 </small>";
+echo "</footer>";
+?>
+</body>
+</html>
