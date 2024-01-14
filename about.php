9acf0a86c80fe5165d152bbcb7051a0b85b6dffe
sufia1117 <122894175+sufia1117@users.noreply.github.com>
2024-01-14 14:55:06 -0500

    ReadAble About Us page

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--     12/04/2023 Sufia Khan
+     Readable About page -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+</head>
+<body>
+    <?php
+        REQUIRE "header.php";
+
+        echo "<p> ReadAble is a one-stop online marketplace for all kinds of exciting new and used books. Our mission is 
+        to provide a quick and accessible way for people to view and order books online. Our website has all different
+        genres and styles of books available. We provide affordable books ranging from non-fiction to high-fantasy. 
+        Readable aims to be competitive and simple so that users have an easy time finding whatever they are looking for.";
+
+        INCLUDE "../footer.php";
+    ?>
+</body>
+</html>
